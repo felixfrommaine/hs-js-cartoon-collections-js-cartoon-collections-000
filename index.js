d3adbccbae171fae090e9993e139891fb7471c04
Felix Xie <felix.xie.i@gmail.com>
2017-07-08 15:04:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 function dwarfRollCall(dwarves) {
+  var stringDwarf = [];
+
+  for (var i = 0; i < dwarves.length; i += 1) {
+    var dwarve = dwarves[i];
+    stringDwarf.push((i + 1) + '. ' + (dwarve) + " ");
+  }
+  return stringDwarf.join('');
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+  var yelledCalls = [];
+
+  for (var i = 0; i < planeteerCalls.length; i += 1) {
+    var planet = planeteerCalls[i];
+
+    yelledCalls.push(planet.toUpperCase() + "!");
+  }
+  return yelledCalls;
 }
 
 function longPlaneteerCalls(words) {
+
+  for (var i = 0; i < words.length; i += 1) {
+    var word = words[i];
+
+    if (word.length > 4) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function findTheCheese (foods) {
-}
+  var cheeses = 'cheddar gouda camembert'.split(' ');
+
+  for (var i = 0; i < foods.length; i += 1) {
+    var food = foods[i];
+
+    if (cheeses.indexOf(food) > -1) {
+      return food;
+    }
+  }
+  return 'no cheese!';
+ }
